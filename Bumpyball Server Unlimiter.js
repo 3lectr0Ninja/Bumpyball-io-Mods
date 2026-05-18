213843ecbbdace189070c84fcfd8023dcfe7a7d7
3lectr0N!nj@ <aaronjs099@gmail.com>
2026-05-18 16:40:06 +0300

    Update Bumpyball Server Unlimiter.js
    All fetch requests and no xhr request

--- Bumpyball Server Unlimiter.js
+++ Bumpyball Server Unlimiter.js
@@ -7,52 +7,6 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pucks.io
 // @grant        none
 // ==/UserScript==
-if(location.pathname=='/'){const proto = XMLHttpRequest.prototype;
- 
-    if (!proto._open) proto._open = proto.open;
-    proto.open = function () {
-        const [method, url] = arguments;
-        Object.assign(this, {method, url});
-        return this._open.apply(this, arguments);
-    };
- 
-    if (!proto._send) proto._send = proto.send;
-    proto.send = function (body) {
-        if (this.url.match(/Listing\?Game=BumpyBall/)) {
-            this._lnd = this.onload;
-            this.onload = function (e) {
-                const table = JSON.parse(new TextDecoder().decode(this.response));
-                table.forEach(server => {
-                    server.MaxPlayers = 999;
-                });
- 
-                Object.defineProperty(this, 'response', {
-                  enumerable: true,
-                  configurable: true,
-                  writable: true,
-                  value: new TextEncoder().encode(JSON.stringify(table)).buffer
-                });
-                this._lnd(e);
-            };
-        }
-        if (this.url.match(/\/Ping/)) {
-            this._lnd = this.onload;
-            this.onload = function (e) {
-                const server = JSON.parse(new TextDecoder().decode(this.response));
-                server.MaxPlayers = 999;
- 
-                Object.defineProperty(this, 'response', {
-                  enumerable: true,
-                  configurable: true,
-                  writable: true,
-                  value: new TextEncoder().encode(JSON.stringify(server)).buffer
-                });
-                this._lnd(e);
-            };
-        }
-        return this._send.apply(this, arguments)
-    };}
-if(location.host=='www.bumpyball.io'&&location.pathname=='/staging/'){
     const originalFetch = window.fetch;
  
     window.fetch = async function (...args) {
@@ -80,4 +34,3 @@ if(location.host=='www.bumpyball.io'&&location.pathname=='/staging/'){
             }
         return response;
     };
-}
